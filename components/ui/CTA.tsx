19758ef37c14556432822af01914f7815b14b452
Shadman Shakib <sm.shakib007@gmail.com>
2024-05-28 22:02:28 +0600

    #Colors updated

--- components/ui/CTA.tsx
+++ components/ui/CTA.tsx
@@ -10,7 +10,7 @@ export default function CTA({ title }: Props) {
       href="https://cal.com/sadman-shakib/30min"
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-block rounded-md bg-[#FF00BF] px-5 py-3 font-semibold text-gray-100
+      className="inline-block rounded-md bg-[#8957e5] px-5 py-3 font-semibold text-gray-100
       shadow-md dark:text-slate-100"
     >
       {title}
