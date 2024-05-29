ea29d2cfe7435c3cceb75eda29cd7fb1952d5835
Shadman Shakib <sm.shakib007@gmail.com>
2024-05-29 22:32:31 +0600

    #Feature section update

--- components/landing/AuthHeader.tsx
+++ components/landing/AuthHeader.tsx
@@ -5,8 +5,8 @@ export default function AuthHeader() {
   return (
     <div className="flex h-full flex-col items-center justify-center rounded-md bg-gray-900">
       <div className="flex items-center justify-center gap-5">
-        <h1 className="text-lg">Next Auth</h1>
         <Image src="/static/images/next-auth-logo.png" width={40} height={40} alt="auth" />
+        <h1 className="text-lg">Next Auth</h1>
       </div>
       <div className="mt-2">
         <p className="text-center text-sm text-gray-400">
